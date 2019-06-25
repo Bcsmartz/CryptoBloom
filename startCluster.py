6d58ec214b51e23c101c024580ea0c1fd1e8c477
Dr. Charles <bczmarts@gmail.com>
2019-06-25 00:49:39 -0400

    add cluster script

--- startCluster.py
+++ startCluster.py
@@ -2,13 +2,22 @@
 
 import os
 import subprocess
-print "start"
-os.system('sh $HOME/Desktop/mine/CryptoBloom/startARQ.sh 70 2 3500 Rai-Cluster')
+
+print "Start Cluster"
+
+crypto_script = os.environ['CRYPTOMINE']
+cpu_use = os.environ['CPU_USE']
+cpu_pri = os.environ['CPU_PRI']
+duration = os.environ['DURATION']
+worker = os.environ['WORKERNAME']
+
+os.system('sh $HOME/Desktop/mine/CryptoBloom/{} {} {} {} {} 2>&1 &'.format(crypto_script, cpu_use, cpu_pri, duration, worker))
+
 #subprocess.check_call("./script.sh '%s'" % arg,   shell=True)
 #sh $HOME/Desktop/mine/CryptoBloom/startARQ.sh 56 1 3000 Rai-Home
 #subprocess.check_call("./home/pi/Desktop/mine/CryptoBloom/startARQ.sh %s %s %s" % (56, 1, 300, 'Rai-Home'), shell=True)
 #subprocess.check_call("./script.ksh %s %s %s" % (agr1, str(arg2), arg3),   shell=True)
 #subprocess.call("./sleep.sh", shell=True)
-print "end"
+print "End Cluster"
 
-#mpiexec -f machinefile -n 2 python runCluster.py
+#mpiexec -f machinefile -n 2 python $HOME/Desktop/mine/CryptoBloom/startCluster.py
---
+++ startCluster.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+echo "$0 cpu_usage cpu_priority timeout_duration worker_name crypto_id"
+echo $0 $1 $2 $3 $4 $5
+
+export CPU_USE=$1
+export CPU_PRI=$2
+export DURATION=$3 #3600
+export WORKERNAME=$4 #"Rai-Homew-13"
+export CRYPTOMINE=$5 #ticker
+
+if [[ "${CRYPTOMINE^^}" = "AEON" ]]; then
+    export CRYPTOMINE='startAeon.sh'
+
+elif [[ "${CRYPTOMINE^^}" = "XMR" ]]; then
+    export CRYPTOMINE='startXmr.sh'
+
+elif [[ "${CRYPTOMINE^^}" = "LOKI" ]]; then
+	export CRYPTOMINE='startLoki.sh'
+
+elif [[ "${CRYPTOMINE^^}" = "XTNC" ]]; then
+    export CRYPTOMINE='startXTNC.sh'
+
+elif [[ "${CRYPTOMINE^^}" = "ARQ" ]]; then
+    export CRYPTOMINE='startARQ.sh'
+
+elif [[ "${CRYPTOMINE^^}" = "XSC" ]]; then
+	export CRYPTOMINE='startXSC.sh'
+
+else
+	echo "No ticker match for... $CRYPTOMINE"
+fi
+
+NODES=2
+
+mpiexec -f $HOME/Deskop/machinefile -n $NODES python $HOME/Desktop/mine/CryptoBloom/startCluster.py
+
