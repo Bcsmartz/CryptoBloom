0fec822c43e9e6cf86e85f9eb8b7570b604c3184
Dr. Charles <bczmarts@gmail.com>
2020-01-12 08:19:10 -0500

    save aeon changes

--- cluster/startClusterAeon.sh
+++ cluster/startClusterAeon.sh
@@ -24,30 +24,33 @@ then
     # Best
 	#echo "divisible by 3"
     timeout "${DURATION}s" ./xmrig -a cryptonight-lite --donate-level 1 \
-    -o pool.aeon.hashvault.pro:3333 -u $AEON_AD -p $WORKERNAME --variant 1 -k \
-    -o aeon.ingest.cryptoknight.cc:5541 -u $AEON_AD.$DIFF -p $WORKERNAME --variant 1 -k \
-    -o aeon.pool.gntl.co.uk:2222 -u $AEON_AD+$DIFF -p $WORKERNAME --variant 1 -k \
-	-o pool.aeonminingpool.com:3333 -u $AEON_AD -p $WORKERNAME --variant 1 -k \
-	-o aeon.pooltupi.com:3333 -u $AEON_AD -p $WORKERNAME --variant 1 -k \
+    -o aeon.herominers.com:10651 -u $AEON_AD -p $WORKERNAME -a k12 -k \
+    -o pool.aeon.hashvault.pro:3333 -u $AEON_AD -p $WORKERNAME -a k12 -k \
+    -o aeon.ingest.cryptoknight.cc:5541 -u $AEON_AD.$DIFF -p $WORKERNAME -a k12 -k \
+    -o aeon.pool.gntl.co.uk:2222 -u $AEON_AD+$DIFF -p $WORKERNAME -a k12 -k \
+	-o pool.aeonminingpool.com:3333 -u $AEON_AD -p $WORKERNAME -a k12 -k \
+	-o aeon.pooltupi.com:3333 -u $AEON_AD -p $WORKERNAME -a k12 -k \
 	-t 1 > $HOME/Desktop/mine/recordAEONs.log &
 elif [ $(( $CURR_HOUR % 2 )) -eq 0 ];          # no need for brackets
 then
 	#echo "divisible by 2"
 	timeout "${DURATION}s" ./xmrig -a cryptonight-lite --donate-level 1 \
-	-o pool.aeonminingpool.com:3333 -u $AEON_AD -p $WORKERNAME --variant 1 -k \
-	-o aeon.ingest.cryptoknight.cc:5541 -u $AEON_AD.$DIFF -p $WORKERNAME --variant 1 -k \
-	-o aeon.pool.gntl.co.uk:2222 -u $AEON_AD+$DIFF -p $WORKERNAME --variant 1 -k \
-	-o pool.aeon.hashvault.pro:3333 -u $AEON_AD -p $WORKERNAME --variant 1 -k \
-	-o aeon.pooltupi.com:3333 -u $AEON_AD -p $WORKERNAME --variant 1 -k \
+	-o aeon.herominers.com:10651 -u $AEON_AD -p $WORKERNAME -a k12 -k \
+	-o pool.aeonminingpool.com:3333 -u $AEON_AD -p $WORKERNAME -a k12 -k \
+	-o aeon.ingest.cryptoknight.cc:5541 -u $AEON_AD.$DIFF -p $WORKERNAME -a k12 -k \
+	-o aeon.pool.gntl.co.uk:2222 -u $AEON_AD+$DIFF -p $WORKERNAME -a k12 -k \
+	-o pool.aeon.hashvault.pro:3333 -u $AEON_AD -p $WORKERNAME -a k12 -k \
+	-o aeon.pooltupi.com:3333 -u $AEON_AD -p $WORKERNAME -a k12 -k \
 	-t 1 > $HOME/Desktop/mine/recordAEONs.log &
 else
     #echo "not divisible - Default"
     timeout "${DURATION}s" ./xmrig -a cryptonight-lite --donate-level 1 \
-	-o aeon.pool.gntl.co.uk:2222 -u $AEON_AD+$DIFF -p $WORKERNAME --variant 1 -k \
-	-o pool.aeon.hashvault.pro:3333 -u $AEON_AD -p $WORKERNAME --variant 1 -k \
-	-o pool.aeonminingpool.com:3333 -u $AEON_AD -p $WORKERNAME --variant 1 -k \
-	-o aeon.ingest.cryptoknight.cc:5541 -u $AEON_AD.$DIFF -p $WORKERNAME --variant 1 -k \
-	-o aeon.pooltupi.com:3333 -u $AEON_AD -p $WORKERNAME --variant 1 -k \
+    -o aeon.herominers.com:10651 -u $AEON_AD -p $WORKERNAME -a k12 -k \
+	-o aeon.pool.gntl.co.uk:2222 -u $AEON_AD+$DIFF -p $WORKERNAME -a k12 -k \
+	-o pool.aeon.hashvault.pro:3333 -u $AEON_AD -p $WORKERNAME -a k12 -k \
+	-o pool.aeonminingpool.com:3333 -u $AEON_AD -p $WORKERNAME -a k12 -k \
+	-o aeon.ingest.cryptoknight.cc:5541 -u $AEON_AD.$DIFF -p $WORKERNAME -a k12 -k \
+	-o aeon.pooltupi.com:3333 -u $AEON_AD -p $WORKERNAME -a k12 -k \
 	-t 1 > $HOME/Desktop/mine/recordAEONs.log &
 fi
 
